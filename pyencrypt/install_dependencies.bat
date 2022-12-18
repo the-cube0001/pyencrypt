d0fba5925c25fdf49c31d8c302aed85c3c3d061a
the-cube0001 <120981804+the-cube0001@users.noreply.github.com>
2022-12-19 08:37:51 +1300

    Delete pyencrypt directory

--- pyencrypt/install_dependencies.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem Install pip, the package manager for Python
-python -m ensurepip --upgrade
-
-rem Install the dependencies listed in requirements.txt
-pip install -r requirements.txt
